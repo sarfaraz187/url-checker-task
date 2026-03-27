ad9692176f54f697305d279a5307d88e487758db
sarfaraz187 <sarfarazmohammed187@gmail.com>
2026-03-27 12:58:05 +0100

    shadcn and tailwind added to frontent

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import "./App.css";
+import { Input } from "@/components/ui/input";
+import "@/App.css";
 
 function App() {
-  return <>React</>;
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center gap-4 p-4">
+      <Input className="w-full max-w-md" placeholder="Enter URL..." />
+    </main>
+  );
 }
 
 export default App;
